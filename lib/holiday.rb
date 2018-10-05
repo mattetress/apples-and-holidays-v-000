c52db0a2c7c402d1a27a5e9f2e75edf18180b15d
Matt Etress <mattetress@me.com>
2018-10-05 21:27:27 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
       name = holiday.to_s.split(" ").each do |word|
         word.capitalize!
       end
-      name.join(" ")
+      name.join(" ").to_s
       supplies = supply_array.join(", ").capitalize!
       binding.pry
       puts "  #{holiday.to_s.split(" ").capitalize!.join(" ")}: #{supply_array.capitalize!.join(", ")}"
