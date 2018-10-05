eaf3c377af0d16b103b2cd187250845479c80496
Matt Etress <mattetress@me.com>
2018-10-05 21:16:05 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, festival_hash|
     puts "#{season.to_s.capitalize}:"
-    season.each do |holiday, supply_array|
+    holiday_hash[season].each do |holiday, supply_array|
       puts "  #{holdiay.to_s.split("_").capitalize!.join(" ")}: #{supply_array.capitalize!.join(", ")}"
     end
   end
